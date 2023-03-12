080d6c58f65bdee2dfc1bc4844cdbe029a3e577d
marimotona <ssmmo.06021224@gmail.com>
2023-03-12 23:52:03 +0900

    app.jsファイルにfirebaseの処理を書く

--- program/app.js
+++ program/app.js
@@ -1,5 +1,35 @@
-let express = require("express");
+import express from "express";
 let app = express();
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, GithubAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOLV540G8tnGmhoc7s_Quf_zoJr-YovMM",
+  authDomain: "team-i-7b23c.firebaseapp.com",
+  projectId: "team-i-7b23c",
+  storageBucket: "team-i-7b23c.appspot.com",
+  messagingSenderId: "86959699193",
+  appId: "1:86959699193:web:964b05f5896b8407a1de28"
+};
+
+// Initialize Firebase
+const firebaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseapp);
+const provider = new GithubAuthProvider();
+const db = getFirestore(firebaseapp);
+provider.addScope("repo");
+
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.sendFile("main.html");
+    
+});
 
 app.listen(8080, () => {
     console.log("Start Server!");
