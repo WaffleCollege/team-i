7cb50d85d5b664ef8b1ef7cd6263866129a8b0e3
marimotona <ssmmo.06021224@gmail.com>
2023-03-15 16:05:46 +0900

    firebase

--- program/public/mainpage/main.js
+++ program/public/mainpage/main.js
@@ -1,5 +1,21 @@
-import { auth, provider } from "./firebase.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+//import { getFirestore } from "firebase/firestore"
 import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
+};
+
+const firebaseapp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseapp);
+//const db = getFirestore(firebaseapp);
+provider.addScope("repo");
 
 //signup
 export async function createAccount() {
@@ -9,7 +25,7 @@ export async function createAccount() {
     console.log (mailAddress, password);
   
   
-    const firebaseResult = await signInWithEmailAndPassword(mailAddress, password)
+    const firebaseResult = await createUserWithEmailAndPassword(auth,mailAddress, password)
       .catch(function(error) {
         alert('ログインできません（' + error.message + '）');
       });
