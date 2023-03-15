fb02b7b659bcb4ee6529a452f7307ca1d115ca0a
marimotona <ssmmo.06021224@gmail.com>
2023-03-16 01:27:04 +0900

    firebase.js階層移動

--- program/public/mainpage/main.js
+++ program/public/mainpage/main.js
@@ -1,11 +1,17 @@
 //import {signInWithEmailAndPassword, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
 //import { signInWithEmailAndPassword, createUserWithEmailAndPassword} from 'firebase/compat/auth';
-import { auth } from '../firebase.js';
+import {auth} from '../../firebase';
+//import { auth } from '../firebaseConfig';
 //import { auth } from '../public/firebase.js';
 //const auth = require("../firebase.js");
 //const auth = getAuth();
 //import 'firebase/compat/auth';
 //import { createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
+//const auth = getAuth();
+
+
+// import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.1/firebase-auth.js";
+// const auth = getAuth();
 //const auth = getAuth();
 
 //signup
